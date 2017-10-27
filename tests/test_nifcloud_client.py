973a51a0dfbdeb614e98c4e7abcd2b8ef06dcdfc
h.omura <omura.hayato@stf.fjct.fujitsu.com>
2017-10-27 17:19:18 +0900

    :+1: add unit tests

---
+++ tests/test_nifcloud_client.py
@@ -0,0 +1,23 @@
+# -*- encoding:utf-8 -*-
+import unittest
+from py_nifcloud.computing_client import ComputingClient
+
+
+class TestNifCloudClientRequest(unittest.TestCase):
+
+    def test_get_computing_describe_regions(self):
+        sut = ComputingClient(region_name="jp-east-1")
+        params = {
+            'Action': 'DescribeRegions',
+        }
+        response = sut.request(method="GET", query=params)
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_computing_describe_regions(self):
+        sut = ComputingClient(region_name="jp-east-1")
+        params = {
+            'Action': 'DescribeRegions',
+        }
+        response = sut.request(method="POST", query=params)
+        self.assertEqual(response.status_code, 200)
+
