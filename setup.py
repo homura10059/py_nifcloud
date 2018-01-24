3ddd8c112c3d15b04e2b9fbe2fb2871077701b25
h.omura <omura.hayato@stf.fjct.fujitsu.com>
2018-01-24 19:10:29 +0900

    :tada: upgrade 0.2.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except IOError:
 
 setup(
     name='py_nifcloud',
-    version='0.1.0',
+    version='0.2.0',
     description='Python wrapper for NifCloud',
     long_description=readme,
     url='https://github.com/o-hayato/py_nifcloud',
