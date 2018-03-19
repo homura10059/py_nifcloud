88f45a119726fd4b9ae2779a752e8f6441deddff
Kazuki Iwata <kazu.0516.k0n0f@gmail.com>
2018-03-19 19:36:43 +0900

    :+1: add NifCloudSigV1Auth class to support SignatureV1

--- py_nifcloud/auth.py
+++ py_nifcloud/auth.py
@@ -32,6 +32,37 @@ class NifCloudSigV2Auth(SigV2Auth):
         return request
 
 
+class NifCloudSigV1Auth:
+
+    def __init__(self, credentials: Credentials):
+        self.credentials = credentials
+
+    def calc_signature(self, params: dict):
+        # Signature を含めて string_to_sign を作成する可能性があるため要素を削除
+        if "Signature" in params.keys():
+            del params["Signature"]
+
+        sorted_params = sorted(params.items())
+        string_to_sign = ""
+        for k, v in sorted_params:
+            string_to_sign += "{key}{value}".format(key=k, value=v)
+        lhmac = hmac.new(self.credentials.secret_key.encode('utf-8'), digestmod=hashlib.sha1)
+        lhmac.update(string_to_sign.encode('utf-8'))
+        return base64.b64encode(lhmac.digest()).strip().decode('utf-8')
+
+    def add_auth(self, request: AWSRequest):
+        params = request.data
+        params['AccessKeyId'] = self.credentials.access_key
+        params['SignatureVersion'] = '1'
+        params['SignatureMethod'] = 'HmacSHA1'
+        params['Timestamp'] = time.strftime(ISO8601, time.gmtime())
+        if self.credentials.token:
+            params['SecurityToken'] = self.credentials.token
+        signature = self.calc_signature(params)
+        params['Signature'] = signature
+        return request
+
+
 class NifCloudSigV0Auth:
 
     def __init__(self, credentials: Credentials):
